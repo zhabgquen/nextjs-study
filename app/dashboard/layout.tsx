a48a9108710959383ae81f9210f95053a320dc36
zhangqien <zqe13939792404@outlook.com>
2024-06-22 22:16:40 +0800

    feat 提交

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+export const experimental_ppr = true; //Next.js将预渲染路径的静态部分，并推迟动态部分，直到用户请求它们。
+//只要你使用Suspense来包装路线的动态部分，Next.js就会知道路线的哪些部分是静态的，哪些部分是动态的。
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
